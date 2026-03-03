ddab41bb9b35b4458ff7549fcb167fc032d30226
KungumapriyaaMKP <kungumapriyaamkp5@gmail.com>
2026-03-03 17:51:36 +0530

    Feat : Location along with vacant space returned

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import cvzone
 import numpy as np
 
 # Video feed
-cap = cv2.VideoCapture('carPark.mp4')
+video_path = 'carPark.mp4'
+location_name = video_path
+cap = cv2.VideoCapture(video_path)
 
 with open('CarParkPos', 'rb') as f:
     posList = pickle.load(f)
 
 width, height = 107, 48
+
+frame_counter = 0  # to control printing frequency
 
 
 def checkParkingSpace(imgPro):
@@ -19,9 +23,7 @@ def checkParkingSpace(imgPro):
         x, y = pos
 
         imgCrop = imgPro[y:y + height, x:x + width]
-        # cv2.imshow(str(x * y), imgCrop)
         count = cv2.countNonZero(imgCrop)
-
 
         if count < 900:
             color = (0, 255, 0)
@@ -32,26 +34,39 @@ def checkParkingSpace(imgPro):
             thickness = 2
 
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
-        cvzone.putTextRect(img, str(count), (x, y + height - 3), scale=1,
-                           thickness=2, offset=0, colorR=color)
 
-    cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(posList)}', (100, 50), scale=3,
-                           thickness=5, offset=20, colorR=(0,200,0))
+    return spaceCounter
+
+
 while True:
 
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
     success, img = cap.read()
+
+    if not success:
+        break
+
+    frame_counter += 1
+
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
-    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                         cv2.THRESH_BINARY_INV, 25, 16)
+    imgThreshold = cv2.adaptiveThreshold(
+        imgBlur, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY_INV,
+        25, 16)
+
     imgMedian = cv2.medianBlur(imgThreshold, 5)
     kernel = np.ones((3, 3), np.uint8)
     imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
 
-    checkParkingSpace(imgDilate)
+    vacant_count = checkParkingSpace(imgDilate)
+
+    # Print once every 30 frames (~1 sec depending on FPS)
+    if frame_counter % 30 == 0:
+        print(f"{location_name} , {vacant_count}")
+
     cv2.imshow("Image", img)
-    # cv2.imshow("ImageBlur", imgBlur)
-    # cv2.imshow("ImageThres", imgMedian)
     cv2.waitKey(10)
